fc6261e297914698b9ef65b1a22876100ef5b4fd
DelphinG3 <42910044+DelphinG3@users.noreply.github.com>
2018-09-02 15:22:28 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+
+Client = discord.Client()
+client = commands.Bot(command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print("Bot is ready and connected to Discord")
+
+@client.event
+async def on_message(message):
+    if message.content.upper().startswith('!PING'):
+        userID = message.author.id
+        await client.send_message(message.channel, "<@%s>  Pong!" % (userID) )
+    if message.content.upper().startswith('!SAY'):
+        args = message.content.split(" ")
+        #args[0] = !SAY
+        #args[1] = Hey
+        #args[2] = There
+        #args[1] = Hey There
+        await client.send_message(message.channel, "%s" % (" ".join(args[1:])))
+
+    if  message.content == "Бот скажи что нибудь":
+        await client.send_message(message.channel, "Виталя ЛОХ")
+    if message.content == "Привет":
+        await client.send_message(message.channel, "Здоровеньки булы")
+    if message.content == "qq":
+        await client.send_message(message.channel, "q")
+    if message.content == "ку":
+        await client.send_message(message.channel, "Дарова")
+    if message.content == "Здарова мыши":
+        await client.send_message(message.channel, "Здарова чёрт")
+
+
+
+
+client.run("NDg1NTAzMzQ5NTE3NzEzNDE4.Dm0jJw.RjBFbnkIQEcPE-zpP6EHTyquzoQ")
+
+
+
+
